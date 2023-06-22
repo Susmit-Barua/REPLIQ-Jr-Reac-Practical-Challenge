a808ddd2e4510261d8893517812e5e21fd965c34
Susmit Barua <111695274+Susmit-Barua@users.noreply.github.com>
2023-06-22 13:08:18 +0600

    Update productList.jsx

--- pages/admin/productList.jsx
+++ pages/admin/productList.jsx
@@ -102,7 +102,7 @@ export default function Productlist({ products }) {
 }
 export const getServerSideProps = async () => {
   const products = await fetchApi(
-    `${baseURL}/products/v2/list?store=US&offset=0&categoryId=4209&limit=2&country=US&sort=freshness&currency=USD&sizeSchema=US&lang=en-US`
+    `${baseURL}/products/v2/list?store=US&offset=0&categoryId=4209&limit=7&country=US&sort=freshness&currency=USD&sizeSchema=US&lang=en-US`
   );
 
   return {
